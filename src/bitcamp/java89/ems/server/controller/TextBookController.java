07c7fc0c83e9f7b82169243924ece758a7a2ad4d
Caroline-Cheon <caroline.jy.cheon@gamil.com>
2016-11-30 11:39:51 +0900

    2016/11/30 RequestParam

--- src/bitcamp/java89/ems/server/controller/TextBookController.java
+++ src/bitcamp/java89/ems/server/controller/TextBookController.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 
 import bitcamp.java89.ems.server.annotation.Component;
 import bitcamp.java89.ems.server.annotation.RequestMapping;
+import bitcamp.java89.ems.server.annotation.RequestParam;
 import bitcamp.java89.ems.server.dao.TextBookDao;
 import bitcamp.java89.ems.server.vo.TextBook;
 
@@ -20,42 +21,51 @@ public class TextBookController {
   }
 
   @RequestMapping(value="textbook/add")
-  public void add(HashMap<String, String> paramMap, PrintStream out) throws Exception {
+  public void add(
+      @RequestParam("title") String title,
+      @RequestParam("author") String author,
+      @RequestParam("press") String press,
+      @RequestParam("releaseDate") int releaseDate,
+      @RequestParam("language") String language,
+      @RequestParam("description") String description,
+      @RequestParam("page") int page,
+      @RequestParam("price") int price,
+      PrintStream out) throws Exception {
     // 주입 받은 contactDao를 사용할 것이기 때문에 더이상 이 메서드에서 ContactDao객체를 준비하지 않는다.
     // => 단 이 메서드가 호출되기 전에 반드시 ContactDao가 주입되어 있어야한다.
-      if (textbookDao.existTitle(paramMap.get("title"))) {
+      if (textbookDao.existTitle(title)) {
         out.println("같은 책이름이 존재합니다. 등록을 취소합니다.");
         return;
       }
       
       TextBook textbook = new TextBook();
-      textbook.setTitle(paramMap.get("title"));
-      textbook.setAuthor(paramMap.get("author"));
-      textbook.setPress(paramMap.get("press"));
-      textbook.setReleaseDate(Integer.parseInt(paramMap.get("releaseDate")));
-      textbook.setLanguage(paramMap.get("language"));
-      textbook.setDescription(paramMap.get("description"));
-      textbook.setPage(Integer.parseInt(paramMap.get("page")));
-      textbook.setPrice(Integer.parseInt(paramMap.get("price")));
+      textbook.setTitle(title);
+      textbook.setAuthor(author);
+      textbook.setPress(press);
+      textbook.setReleaseDate(releaseDate);
+      textbook.setLanguage(language);
+      textbook.setDescription(description);
+      textbook.setPage(page);
+      textbook.setPrice(price);
       
       textbookDao.insert(textbook);
       out.println("등록하였습니다.");
   }
   
   @RequestMapping(value="textbook/delete")
-  public void delete(HashMap<String, String> paramMap, PrintStream out) throws Exception {
+  public void delete(@RequestParam("title") String title, PrintStream out) throws Exception {
     // 주입 받은 contactDao를 사용할 것이기 때문에 더이상 이 메서드에서 ContactDao객체를 준비하지 않는다.
     // => 단 이 메서드가 호출되기 전에 반드시 ContactDao가 주입되어 있어야한다.
-      if (!textbookDao.existTitle(paramMap.get("title"))) {
+      if (!textbookDao.existTitle(title)) {
         out.println("해당 데이터가 없습니다");
         return;
       }
-      textbookDao.delete(paramMap.get("title"));
+      textbookDao.delete(title);
       out.println("해당 데이터 삭제 완료하였습니다.");
   }
   
   @RequestMapping(value="textbook/list")
-  public void list(HashMap<String, String> paramMap, PrintStream out) throws Exception {
+  public void list(PrintStream out) throws Exception {
     // 주입 받은 contactDao를 사용할 것이기 때문에 더이상 이 메서드에서 ContactDao객체를 준비하지 않는다.
     // => 단 이 메서드가 호출되기 전에 반드시 ContactDao가 주입되어 있어야한다.
       ArrayList<TextBook> list = textbookDao.getList();
@@ -73,33 +83,42 @@ public class TextBookController {
   }
   
   @RequestMapping(value="textbook/update")
-  public void update(HashMap<String, String> paramMap, PrintStream out) throws Exception {
+  public void update(
+      @RequestParam("title") String title,
+      @RequestParam("author") String author,
+      @RequestParam("press") String press,
+      @RequestParam("releaseDate") int releaseDate,
+      @RequestParam("language") String language,
+      @RequestParam("description") String description,
+      @RequestParam("page") int page,
+      @RequestParam("price") int price,
+      PrintStream out) throws Exception {
     // 주입 받은 contactDao를 사용할 것이기 때문에 더이상 이 메서드에서 ContactDao객체를 준비하지 않는다.
     // => 단 이 메서드가 호출되기 전에 반드시 ContactDao가 주입되어 있어야한다.
-      if (!textbookDao.existTitle(paramMap.get("title"))) {
+      if (!textbookDao.existTitle(title)) {
         out.println("책이름을 찾지 못했습니다.");
         return;
       }
       
       TextBook textbook = new TextBook();
-      textbook.setTitle(paramMap.get("title"));
-      textbook.setAuthor(paramMap.get("author"));
-      textbook.setPress(paramMap.get("press"));
-      textbook.setReleaseDate(Integer.parseInt(paramMap.get("releaseDate")));
-      textbook.setLanguage(paramMap.get("language"));
-      textbook.setDescription(paramMap.get("description"));
-      textbook.setPage(Integer.parseInt(paramMap.get("page")));
-      textbook.setPrice(Integer.parseInt(paramMap.get("price")));
+      textbook.setTitle(title);
+      textbook.setAuthor(author);
+      textbook.setPress(press);
+      textbook.setReleaseDate(releaseDate);
+      textbook.setLanguage(language);
+      textbook.setDescription(description);
+      textbook.setPage(page);
+      textbook.setPrice(price);
       
       textbookDao.update(textbook);
       out.println("변경하였습니다.");
   }
   
   @RequestMapping(value="textbook/view")
-  public void view(HashMap<String, String> paramMap, PrintStream out) throws Exception {
+  public void view(@RequestParam("title") String title, PrintStream out) throws Exception {
     // 주입 받은 contactDao를 사용할 것이기 때문에 더이상 이 메서드에서 ContactDao객체를 준비하지 않는다.
     // => 단 이 메서드가 호출되기 전에 반드시 ContactDao가 주입되어 있어야한다.
-      ArrayList<TextBook> list = textbookDao.getListByTitle(paramMap.get("title"));
+      ArrayList<TextBook> list = textbookDao.getListByTitle(title);
       for (TextBook textbook : list) {
         out.println("------------------------------");
         out.printf("책이름: %s\n", textbook.getTitle());
